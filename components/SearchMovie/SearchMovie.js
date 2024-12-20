9354a442314d2481ca2898028928e4742a4c9fe0
ffnoor <faizan.fznoor@gmail.com>
2024-12-20 19:15:25 +0500

    refactor: Reduce image size

--- components/SearchMovie/SearchMovie.js
+++ components/SearchMovie/SearchMovie.js
@@ -17,7 +17,7 @@ const SearchMovie = ({ searchMovie, query }) => {
                                     <div className={styles.thumbnail}>
                                         {movie.poster_path ?
                                             <Image
-                                                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
                                                 alt={movie.title}
                                                 width={140}
                                                 height={210}
